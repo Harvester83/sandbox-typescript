27848c1e7946eee30db12671915c24b5b58c5efa
Rashad <rashad.suleymanov83@gmail.com>
2026-07-10 18:02:01 +0400

    chapter:(algorithms) two sum O(n)

--- src/two-sum.ts
+++ src/two-sum.ts
@@ -1,16 +1,34 @@
-const nums: number[] = [2,7,11,15];
-const target: number = 26;
+const nums: number[] = [2, 7, 11, 15];
+const target: number = 9;
+
+// function twoSum(nums: number[], target) {
+//      for (let i = 0; i < nums.length; i++) {
+//         for(let j = i + 1; j < nums.length; j++) {
+//            if (nums[i] + nums[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//      }
+
+//      return
+// };
+
+
 
 function twoSum(nums: number[], target: number): number[] {
-     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+    const map = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        const need = target - nums[i];
+
+        if (map.has(need)) {
+            return [map.get(need)!, i];
         }
+
+        map.set(nums[i], i);
     }
 
     return [];
-};
+}
 
-console.log(twoSum(nums, target))
+console.log(twoSum(nums, target));
